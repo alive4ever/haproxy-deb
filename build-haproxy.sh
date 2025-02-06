1159030f5c20f624e34aca853107e803fa374105
alive4ever <alive4ever@users.noreply.github.com>
2025-02-06 00:58:13 +0000

    haproxy: bump to 3.0.8
    Bump to version 3.0.8 and use deb-src instead of manually typed urls to
    use apt to download sources.

--- build-haproxy.sh
+++ build-haproxy.sh
@@ -1,18 +1,21 @@
 export DEBFULLNAME="alive4ever"
 export DEBEMAIL="alive4ever@users.noreply.github.com"
+export HAPROXY_VERSION="3.0.8"
+export PPA_VERSION="1ppa1~jammy"
+export BACKPORT_VERSION="1bpo1"
 mkdir -p ~/build
 cd ~/build
-haproxy_dsc="https://launchpad.net/~vbernat/+archive/ubuntu/haproxy-3.0/+sourcefiles/haproxy/3.0.7-1ppa1~jammy/haproxy_3.0.7-1ppa1~jammy.dsc"
-haproxy_srctarball="https://launchpad.net/~vbernat/+archive/ubuntu/haproxy-3.0/+sourcefiles/haproxy/3.0.7-1ppa1~jammy/haproxy_3.0.7.orig.tar.gz"
-haproxy_debiantarball="https://launchpad.net/~vbernat/+archive/ubuntu/haproxy-3.0/+sourcefiles/haproxy/3.0.7-1ppa1~jammy/haproxy_3.0.7-1ppa1~jammy.debian.tar.xz"
-for url in "$haproxy_dsc" "$haproxy_srctarball" "$haproxy_debiantarball" ; do
-	curl -LO "$url"
-done
-dpkg-source -x haproxy_3.0.7-1ppa1~jammy.dsc
-cd haproxy-3.0.7
+curl -L "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3d653970fbab0a890e4e4e9a0f14d8b0cf4efe96" | sudo tee /etc/apt/keyrings/haproxy-ppa.asc
+cat << EOL | sudo tee /etc/apt/sources.list.d/haproxy-ppa.list
+deb-src [signed-by=/etc/apt/keyrings/haproxy-ppa.asc] https://ppa.launchpadcontent.net/vbernat/haproxy-3.0/ubuntu jammy main
+EOL
+sudo apt update
+apt source --download-only haproxy
+dpkg-source -x haproxy_${HAPROXY_VERSION}-${PPA_VERSION}.dsc
+cd haproxy-${HAPROXY_VERSION}
 sudo apt build-dep -y .
-	dch --newversion 3.0.7-2bpo2 --distribution bookworm "Rebuild for bookworm"
-	debuild -us -uc
+dch -b --newversion ${HAPROXY_VERSION}-${BACKPORT_VERSION} --distribution bookworm "Rebuild for bookworm"
+debuild -us -uc
 cd ..
 mkdir -p /tmp/hosttmp/haproxy_deb
 cp -v *.deb /tmp/hosttmp/haproxy_deb
